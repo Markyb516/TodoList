b35afbe46b513af3643a83beb38c37b759c643da
Markus <Markus.buchanan1125@gmail.com>
2024-10-05 15:35:24 -0400

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Markus Buchanan on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
